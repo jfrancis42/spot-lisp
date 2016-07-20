04db463cc1fb04c31bd1e3b468cb03a1adf3535c
Jeff Francis <jeff@gritch.org>
2016-07-19 20:12:00 -0700

    First push.

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # spot-lisp
 A Common Lisp client for reading location data from the public findmespot.com API.
+
+Fill in the value for \*glld\* (which is supplied by the Spot web page when you create a shared page), the value for \*feedpassword\* (if you supplied one for the shared page), and the Google Geocoder API key (if you want to be able to do reverse geocoding).  If you don't have a password for your shared location page, set the \*feedpassword\* to nil (not "nil").
+
+Start the reader thread with (start-spot).  This will update the \*locations\* variable with a list of the last fifty location objects, updating the list every 2.5 minutes (the maximum update rate allowed by the free API).  There's a mutex lock for \*locations\* called \*locations-lock\*, which it's wise to use when accessing \*locations\* (but technically not mandatory).
+
+If you want to sit and watch the position data roll in, run (show-spots).  This will start an endless loop that updates every time new data shows up.
+
+That's it.  Plain and simple.  Obviously, you can do far more with the information than simply display it, but this solves my specific problem quickly and easily.  It could probably do with more error detection (for network errors, or for lat/lon combinations that fail to reverse geocode properly), but it's good enough for now.
+
+Feel free to use and adapt.
+
---
+++ spot.lisp
@@ -0,0 +1,161 @@
+(ql:quickload :drakma)
+(ql:quickload :cl-json)
+(ql:quickload :local-time)
+(ql:quickload :bordeaux-threads)
+(ql:quickload :quicklisp-slime-helper)
+
+(defparameter *glld* "PlaceYourGLLDThatSpotGivesYouHere")
+(defparameter *google-api-key* "PlaceYourGoogleGeocoderAPIKeyHere")
+(defparameter *feedpassword* "PlaceYourSpotSharedSitePasswordHere(orNilIfNoPassword)")
+(defparameter *sleeptime* 30)
+(defparameter *locations* nil)
+(defparameter *locations-lock* (bt:make-lock))
+(defparameter *uptodate-thread* nil)
+
+(defun bytes-to-ascii (bytelist)
+  "Turn a list of bytes into string (for some very annoying reason,
+drakma:http-request returns the body as a list of bytes instead of a
+string)."
+  (map 'string #'code-char bytelist))
+
+(defun lookup-location (loc)
+  "Use the Google Geocoding API to do a reverse geocode lookup (ie,
+convert lat lon to address)."
+  (let* ((url (concatenate 'string "https://maps.googleapis.com/maps/api/geocode/json?latlng="
+			   (format nil "~A" (latitude loc)) ","
+			   (format nil "~A" (longitude loc))
+			   "&key=" *google-api-key*))
+	 (result (drakma:http-request url
+				      :method :get
+				      :accept "application/json"
+				      :content-type "application/json")))
+    (if (> (length result) 0)
+	(json:decode-json-from-string (bytes-to-ascii 
+				       (nth-value 0 result)))
+	nil)))
+
+(defun lat-lon-to-street-address (lat lon)
+  "Convert an arbitrary lat/lon into a street address."
+  (street-address (lookup-location (make-instance 'location :latitude lat :longitude lon))))
+
+(defun street-address (geocoded-result)
+  "Extract the formatted street address from (lookup-location)
+result."
+  (cdr (assoc :formatted--address (cadar geocoded-result))))
+
+(defun get-spot-locations (glld &optional (passwd nil))
+  "Get the last fifty entries as JSON data from the Spot server.
+Accepts an optional password for password-protected location feeds."
+  (let ((feed (concatenate 'string "https://api.findmespot.com/spot-main-web/consumer/rest-api/2.0/public/feed/"
+			   glld "/message.json")))
+    (if passwd (setf feed (concatenate 'string feed "?feedPassword=" passwd)))
+    (let ((result (drakma:http-request feed
+                                       :method :get
+                                       :accept "application/json"
+                                       :content-type "application/json")))
+      (if (> (length result) 0)
+          (json:decode-json-from-string (bytes-to-ascii 
+                                         (nth-value 0 result)))
+          nil))))
+
+(defun extract-spot-locations (spot-json)
+  "Extract just the location data from the parsed JSON object returned
+from the Spot API."
+  (cdr (car (cdr (assoc :messages (cdr (car (cdr (car spot-json)))))))))
+
+(defun create-location-objects-from-list (location-list)
+  "Takes a list of JSON locations (usually
+from (extract-spot-locations)) and returns a list of location
+objects."
+  (map 'list #'(lambda (n) (make-location n)) location-list))
+
+(defun sort-locations (location-list)
+  "Sort a list of location objects."
+  (let ((location-list-copy (copy-list location-list)))
+    (sort location-list-copy #'<
+          :key #'(lambda (n) (unix-time n)))))
+
+(defclass location ()
+  ((id :accessor id
+         :initarg :id
+         :initform nil)
+   (messenger-id :accessor messenger-id
+                 :initarg :messenger-id
+                 :initform nil)
+   (unix-time :accessor unix-time
+        :initarg :unix-time
+        :initform nil)
+   (message-type :accessor message-type
+        :initarg :message-type
+        :initform nil)
+   (latitude :accessor latitude
+        :initarg :latitude
+        :initform nil)
+   (longitude :accessor longitude
+        :initarg :longitude
+        :initform nil)
+   (model-id :accessor model-id
+        :initarg :model-id
+        :initform nil)
+   (show-custom-msg :accessor show-custom-msg
+        :initarg :show-custom-msg
+        :initform nil)
+   (date-time :accessor date-time
+        :initarg :date-time
+        :initform nil)
+   (battery-state :accessor battery-state
+        :initarg :battery-state
+        :initform nil)
+   (hidden :accessor hidden
+        :initarg :hidden
+        :initform nil)
+   ))
+
+(defmethod make-location (l)
+  "Turn a JSON location into a location object."
+  (make-instance 'location
+                 :id (cdr (assoc :id l))
+                 :messenger-id (cdr (assoc :messenger-id l))
+                 :unix-time (cdr (assoc :unix-time l))
+                 :message-type (cdr (assoc :message-type l))
+                 :latitude (cdr (assoc :latitude l))
+                 :longitude (cdr (assoc :longitude l))
+                 :model-id (cdr (assoc :model-id l))
+                 :show-custom-msg (cdr (assoc :show-custom-msg l))
+                 :date-time (cdr (assoc :date-time l))
+                 :battery-state (cdr (assoc :battery-state l))
+                 :hidden (cdr (assoc :hidden l))
+                 ))
+
+(defmethod pp ((n location))
+  "Pretty print a location."
+  (format nil "type:~A time:~A lat:~A lon:~A"
+          (message-type n) (local-time:unix-to-timestamp (unix-time n)) (latitude n) (longitude n)))
+
+(defun up-to-dater ()
+  "Keep the data structure up to date."
+  (loop
+     (bt:with-lock-held (*locations-lock*)
+       (setf *locations*
+             (sort-locations
+              (create-location-objects-from-list
+               (extract-spot-locations
+                (get-spot-locations *glld* *feedpassword*))))))
+     (sleep (* 60 2.5))))
+
+(defun start-spot ()
+  "Start the thread that reads the latest API data from spot.com every
+2.5 minutes. This keeps the *locations* list up-to-date."
+  (setf *uptodate-thread* (bt:make-thread (lambda () (up-to-dater)) :name "up-to-dater"))
+  (format t "spot update thread is running...~%"))
+
+(defun watch-spots ()
+  "Loop forever and print each new Spot location as it rolls in."
+  (let ((old-loc nil) (new-loc t))
+    (loop
+       (bt:with-lock-held (*locations-lock*) (setf new-loc (pp (first (last *locations*))))
+			  (if (not (equal old-loc new-loc))
+			      (progn
+				(setf old-loc new-loc)
+				(format t "~A ~A~%" new-loc (street-address (lookup-location (first (last *locations*)))) ))))
+       (sleep *sleeptime*))))
